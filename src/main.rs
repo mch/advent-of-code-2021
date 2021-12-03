bf06096781d918a0df4672791663baa0fc7a51fd
Matt Hughes <matt.hughes@arterys.com>
2021-12-02 20:47:23 -0700

    Rearranging things slightly, adding day 2 data

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,49 @@
 use std::fs;
 use std::error::Error;
 
+// Box<dyn Error>???
+fn main() -> Result<(), Box<dyn Error>> {
+    day1()
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Day 1
+
+fn day1() -> Result<(), Box<dyn Error>> {
+    // How can I clone a String inside a Result<String, io::Error>,
+    // where io::Error doesn't implement Clone?
+    let input: String = fs::read_to_string("aoc-2021-puzzle1.txt").unwrap();
+    let input1 = input.clone();
+    let input2 = input.clone();
+    let input3 = input.clone();
+    let ups = count_larger_measurements(input1);
+    println!("num single measurement ups: {}", ups);
+
+    let window_ups = count_larger_measurement_window(input2);
+    println!("num windowsed measurement ups: {}", window_ups);
+
+    let numbers: Vec<u32> = parse_lines(&input3);
+
+    // I don't understand why numbers_iter has to be mutable. Is it because next()
+    // mutates self? If so, how do for loops etc get away with it? Do they make
+    // mutable copies?
+    let mut numbers_iter = numbers.iter();
+    let first_number: Option<&u32> = numbers_iter.next();
+    let accumulator: (u32, u32) = (0, first_number.unwrap_or(&0).clone()); //yolo
+    let result = numbers_iter.fold(accumulator, |accumulator: (u32, u32), number: &u32| {
+        let (ups, previous) = accumulator;
+        if *number > previous {
+             (ups+1, *number)
+        } else {
+            (ups, *number)
+        }
+    });
+    let (ups, previous) = result;
+    println!("fold result: ({}, {})", ups, previous);
+
+    Ok(())
+}
+
 fn count_larger_measurements(input: String) -> u32 {
     let mut ups: u32 = 0;
     let mut lines = input.lines();
@@ -40,42 +83,6 @@ fn count_larger_measurement_window(input: String) -> u32 {
     ups
 }
 
-// Box<dyn Error>???
-fn main() -> Result<(), Box<dyn Error>> {
-    // How can I clone a String inside a Result<String, io::Error>,
-    // where io::Error doesn't implement Clone?
-    let input: String = fs::read_to_string("aoc-2021-puzzle1.txt").unwrap();
-    let input1 = input.clone();
-    let input2 = input.clone();
-    let input3 = input.clone();
-    let ups = count_larger_measurements(input1);
-    println!("num single measurement ups: {}", ups);
-
-    let window_ups = count_larger_measurement_window(input2);
-    println!("num windowsed measurement ups: {}", window_ups);
-
-    let numbers: Vec<u32> = parse_lines(&input3);
-
-    // I don't understand why numbers_iter has to be mutable. Is it because next()
-    // mutates self? If so, how do for loops etc get away with it? Do they make
-    // mutable copies?
-    let mut numbers_iter = numbers.iter();
-    let first_number: Option<&u32> = numbers_iter.next();
-    let accumulator: (u32, u32) = (0, first_number.unwrap_or(&0).clone()); //yolo
-    let result = numbers_iter.fold(accumulator, |accumulator: (u32, u32), number: &u32| {
-        let (ups, previous) = accumulator;
-        if *number > previous {
-             (ups+1, *number)
-        } else {
-            (ups, *number)
-        }
-    });
-    let (ups, previous) = result;
-    println!("fold result: ({}, {})", ups, previous);
-
-    Ok(())
-}
-
 fn parse_lines(input: &str) -> Vec<u32>{
     let mut ugh: Vec<u32> = Vec::new();
     for line in input.lines() {
@@ -87,11 +94,24 @@ fn parse_lines(input: &str) -> Vec<u32>{
     ugh
 }
 
+////////////////////////////////////////////////////////////////////////////////
+// Day 2
+////////////////////////////////////////////////////////////////////////////////
+
+fn day2() {
+
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Tests
+////////////////////////////////////////////////////////////////////////////////
+
+
 #[cfg(test)]
 mod tests {
-    // Note this useful idiom: importing names from outer (for mod tests) scope.
     use super::*;
 
+    // Day 1 tests
     #[test]
     fn test_parsing_ints() {
         assert_eq!(parse_lines("1\n34\n43\n"), vec![1, 34, 43]);
@@ -102,4 +122,7 @@ mod tests {
         assert_eq!(parse_lines("1\nHi\n43\n"), vec![1, 43]);
 
     }
+
+    // Day 2 tests
+
 }
