519fc9188d4bd30ed9c206acb7710e63e182249e
Matt Hughes <matt.c.hughes@gmail.com>
2021-12-05 06:14:01 -0700

    day5 placeholder

---
+++ src/day5.rs
@@ -0,0 +1,23 @@
+use std::fs;
+use std::error::Error;
+
+pub fn puzzle1() -> Result<(), Box<dyn Error>> {
+    let input: String = fs::read_to_string("data/day1-input.txt").unwrap();
+
+    Ok(())
+}
+
+mod tests {
+
+    #[test]
+    fn test_day5_foo() {
+        println!("A test!");
+        assert_eq!(0, 1);
+    }
+
+    #[test]
+    fn test_day5_parse_line() {
+        let input = "48,233 -> 48, 456";
+        
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod day1;
 mod day2;
 mod day3;
 mod day4;
+mod day5;
 
 // Box<dyn Error>???
 fn main() -> Result<(), Box<dyn Error>> {
@@ -12,6 +13,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     //day2::day2p2()
     //day3::puzzle1()
     //day3::puzzle2()
-    day4::puzzle1_and_2()
+    //day4::puzzle1_and_2()
+    day5::puzzle1()
 }
 
