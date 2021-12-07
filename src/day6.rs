4c9d2d2d1e52e6cd79a911dfb5c684eaf22363cf
Matt Hughes <matt.c.hughes@gmail.com>
2021-12-07 05:28:44 -0700

    day 6 puzzle 2

--- src/day6.rs
+++ src/day6.rs
@@ -18,16 +18,34 @@ pub fn puzzle1() -> () {
         })
         .collect();
 
-    // let mut fish_timers: Vec<i32> = vec!(3,4,3,1,2);
+    //let mut fish_timers: Vec<i32> = vec!(3,4,3,1,2);
 
-    let days = 80;
-    for _ in 0..days {
-        fish_timers = fish_step(fish_timers);
+    let solution = 2;
+    if solution == 1 {
+        // Track each individual fish
+        let days = 256;
+        let mut old_num_fish = fish_timers.len();
+        for day in 0..days {
+            fish_timers = fish_step(fish_timers);
+            println!("After {} days, there are {} fish, increase of {}", day, fish_timers.len(), fish_timers.len() - old_num_fish);
+            old_num_fish = fish_timers.len();
+        }
+        println!("After {} days, there are {} fish.", days, fish_timers.len());
+    } else if solution == 2 {
+        // Track number of fish each day instead
+        let mut fish_per_day = convert_individual_fish_to_number_per_day(&fish_timers);
+        let days = 256;
+        for day in 0..days {
+            time_step_fishes(&mut fish_per_day);
+            //println!("{:?}", fish_per_day);
+        }
+        let total = fish_per_day.into_iter().sum::<usize>();
+        println!("After {} days there are a total of {} fish.", days, total);
     }
-    println!("After {} days, there are {} fish.", days, fish_timers.len());
+    
 }
 
-fn fish_step(fish_timers: Vec<i32>) -> Vec<i32> {
+fn fish_step(mut fish_timers: Vec<i32>) -> Vec<i32> {
     let mut new_fish_timers = Vec::new();
     let mut number_of_new_fish = 0;
     for fish_timer in fish_timers {
@@ -132,4 +150,97 @@ mod tests {
 
         assert_eq!(vec!(1,2,1,6,0,8), fishes_timer);
     }
+
+    #[test]
+    fn day6_track_num_fish_per_day() {
+        // This vector tracks the number of fish on each day, where the index of the vector is the day.
+        // So if we have 5 fish, with timers of 3, 4, 3, 1, 2, 
+        // Then we have fish_days of: 
+        // 0, 1, 1, 2, 1
+        let mut fish_days: Vec<usize> = vec![0; 9];
+        fish_days[1] = 1;
+
+        time_step_fishes(&mut fish_days);
+        assert_eq!(vec!(1, 0, 0, 0, 0, 0, 0, 0, 0), fish_days);
+
+        time_step_fishes(&mut fish_days);
+        assert_eq!(vec!(0, 0, 0, 0, 0, 0, 1, 0, 1), fish_days);
+
+        time_step_fishes(&mut fish_days);
+        assert_eq!(vec!(0, 0, 0, 0, 0, 1, 0, 1, 0), fish_days);
+
+        time_step_fishes(&mut fish_days);
+        assert_eq!(vec!(0, 0, 0, 0, 1, 0, 1, 0, 0), fish_days);
+
+        time_step_fishes(&mut fish_days);
+        assert_eq!(vec!(0, 0, 0, 1, 0, 1, 0, 0, 0), fish_days);
+
+        time_step_fishes(&mut fish_days);
+        assert_eq!(vec!(0, 0, 1, 0, 1, 0, 0, 0, 0), fish_days);
+
+        time_step_fishes(&mut fish_days);
+        assert_eq!(vec!(0, 1, 0, 1, 0, 0, 0, 0, 0), fish_days);
+
+        time_step_fishes(&mut fish_days);
+        assert_eq!(vec!(1, 0, 1, 0, 0, 0, 0, 0, 0), fish_days);
+
+        time_step_fishes(&mut fish_days);
+        assert_eq!(vec!(0, 1, 0, 0, 0, 0, 1, 0, 1), fish_days);
+    }
+
+    #[test]
+    fn day6_track_num_fish_per_day_test2() {
+        // This vector tracks the number of fish on each day, where the index of the vector is the day.
+        // So if we have 5 fish, with timers of 3, 4, 3, 1, 2, 
+        // Then we have fish_days of: 
+        // 0, 1, 1, 2, 1
+        let mut fish_days: Vec<usize> = vec![0; 9];
+        fish_days[1] = 1;
+        fish_days[2] = 1;
+        fish_days[3] = 2;
+        fish_days[4] = 1;
+
+        let days = 18;
+        for _ in 0..18 {
+            time_step_fishes(&mut fish_days);
+            println!("{:?}", fish_days);
+        }
+
+        // I don't understand what isn't working about this: 
+        // fish_days.fold(0, |s, x| x + s)
+        // or this:
+        //let argh: Vec<i32> = vec!(1,2,3,4);
+        //let s = argh.sum();
+        let mut total = 0;
+        for num_fish in fish_days {
+            total += num_fish;
+        }
+        println!("After {} days, there is a total of {} fish", days, total);
+    }
+
+    #[test]
+    fn day6_track_days_convert() {
+                // So if we have 5 fish, with timers of 3, 4, 3, 1, 2, 
+        // Then we have fish_days of: 
+        // 0, 1, 1, 2, 1
+        let mut fish_timers: Vec<i32> = vec!(3,4,3,1,2);
+        assert_eq!(vec![0,1,1,2,1,0,0,0,0], convert_individual_fish_to_number_per_day(&mut fish_timers));
+    }
+}
+
+fn time_step_fishes(num_fish_each_day: &mut Vec<usize>) -> () {
+    let num_day_zero_fish = num_fish_each_day[0];
+    for index in 0..8 {
+        num_fish_each_day[index] = num_fish_each_day[index + 1];
+    }
+    num_fish_each_day[8] = num_day_zero_fish;
+    num_fish_each_day[6] += num_day_zero_fish;
+}
+
+fn convert_individual_fish_to_number_per_day(fish_timers: &Vec<i32>) -> Vec<usize> {
+    let mut fish_per_day: Vec<usize> = vec![0; 9];
+    for fish_timer in fish_timers {
+        fish_per_day[*fish_timer as usize] += 1;
+    }
+    fish_per_day
 }
