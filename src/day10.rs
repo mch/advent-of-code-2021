ceae12491fc310b403e992b9c0c6ad60c925314a
Matt Hughes <matt.c.hughes@gmail.com>
2021-12-12 07:19:03 -0700

    more day10

--- src/day10.rs
+++ src/day10.rs
@@ -19,14 +19,23 @@ fn parse_input(input: &str) -> ParseResult {
             let opening_char = stack.pop().unwrap();
             let index = opening_tokens.iter().position(|&item| item == opening_char).unwrap();
             let matching_closing_char = closing_tokens[index];
-            if c == matching_closing_char {
-                return ParseResult::Chunk(opening_char)
-            } else {
+            if c != matching_closing_char {
                 return ParseResult::Corrupt(c)
             }
         }
     }
     ParseResult::Chunk(input.chars().nth(0).unwrap())
+}
+
+fn score_syntax_error(result: &ParseResult) -> i32 {
+    match *result {
+        ParseResult::Chunk(_) => 0,
+        ParseResult::Corrupt(')') => 3,
+        ParseResult::Corrupt(']') => 57,
+        ParseResult::Corrupt('}') => 1197,
+        ParseResult::Corrupt('>') => 25137,
+        ParseResult::Corrupt(_) => 0,
+    }
 }
 
 mod tests {
@@ -46,5 +55,31 @@ mod tests {
         assert_eq!(ParseResult::Corrupt(')'), parse_input("[)"));
         assert_eq!(ParseResult::Corrupt('>'), parse_input("{>"));
         assert_eq!(ParseResult::Corrupt('}'), parse_input("<}"));
+    }
+
+    #[test]
+    fn day10_parse_nested_chunks() {
+        assert_eq!(ParseResult::Chunk('('), parse_input("([<{}>])"));
+    }
+
+    #[test]
+    fn day10_parse_nested_corrupt_chunks() {
+        assert_eq!(ParseResult::Corrupt('}'), parse_input("{([(<{}[<>[]}>{[]{[(<()>"));
+        assert_eq!(ParseResult::Corrupt(')'), parse_input("[[<[([]))<([[{}[[()]]]"));
+        assert_eq!(ParseResult::Corrupt(']'), parse_input("[{[{({}]{}}([{[{{{}}([]"));
+        assert_eq!(ParseResult::Corrupt(')'), parse_input("[<(<(<(<{}))><([]([]()"));
+        assert_eq!(ParseResult::Corrupt('>'), parse_input("<{([([[(<>()){}]>(<<{{"));
+    }
+
+    #[test]
+    fn day10_syntax_error_score() {
+        let results = vec![
+            ParseResult::Corrupt('}'),
+            ParseResult::Corrupt(')'),
+            ParseResult::Corrupt(']'),
+            ParseResult::Corrupt(')'),
+            ParseResult::Corrupt('>'),
+        ];
+        assert_eq!(26397, results.iter().map(score_syntax_error).sum());
     }
 }
