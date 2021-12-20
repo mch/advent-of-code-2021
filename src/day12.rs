2cc00f2f1bcd9def2324014f80682c167bf6e93f
Matt Hughes <matt.c.hughes@gmail.com>
2021-12-20 06:11:19 -0700

    day 12 start

--- src/day12.rs
+++ src/day12.rs
@@ -1,3 +1,34 @@
 pub fn puzzle() {
+    // Load cave connectivity graph
+    // Find all routes through the graph
+    // Visit small caves only once
+}
+
+pub fn find_paths(graph: &Graph, start: &Node, end: &Node) -> Vec<Path> {
+    let mut paths: Vec<Path> = Vec::new();
+
+    // 
+
+    paths
+}
+
+struct Graph {
     
+}
+
+struct Node {
+    
+}
+
+struct Path {
+    
+}
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn day12_cave_paths() {
+        
+    }
 }
